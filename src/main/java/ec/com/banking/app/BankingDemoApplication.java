33dd9989d2ff3cb4329b7f2e22ebb73328fb9886
Mishell Yagual <mcym_1995@hotmail.com>
2025-09-21 10:05:27 -0500

    build: Initial commit

---
+++ src/main/java/ec/com/banking/app/BankingDemoApplication.java
@@ -0,0 +1,13 @@
+package ec.com.banking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/banking/app/BankingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.banking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
